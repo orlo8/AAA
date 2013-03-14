5f752197dc505530af8ddae9100edaed1151bf5f
Andre Liima <Andreliima147@gmail.com>
2013-03-14 03:42:20 +0200

    progress indicator

--- war/js/loadcontent.js
+++ war/js/loadcontent.js
@@ -1,10 +1,19 @@
 $(document).ready(function(){
+	var loadingImage = new Image();
+	loadingImage.src = "ajax-loader.gif"
 	$('#peamenyy').on("click","a",function(){
 		var href = $(this).attr('href');
 		$('#peamenyy a').removeClass("aktiivne");
 		$(this).addClass("aktiivne");
 		if(href=="index.html"){
 			$('#sisu').load(href+" #sisu");
+			return false;
+		}
+		else if (href == "tulemused.html"){
+			$('#sisu').empty().html('<img id="ajaxLoader" src="ajax-loader.gif" />')
+			setTimeout(function(){
+				$('#sisu').load(href+" #sisu");
+			},1000);
 			return false;
 		}
 		$('#sisu').load(href);
